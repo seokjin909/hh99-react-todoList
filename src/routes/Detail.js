0be163c6eee5f5c4d323e83a43597fb36c9b3466
jin <thseokjin@naver.com>
2023-08-25 17:07:42 +0900

    TodoList C1

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,39 @@
 import React from "react";
 import { connect } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Card = styled.div`
+  margin: auto;
+  width: 300px;
+  height: 200px;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 function Detail({ toDos }) {
   const myId = useParams().id;
   const toDo = toDos.find((toDo) => toDo.id === parseInt(myId));
 
   return (
-    <>
-      {toDo?.text}
-      Created at: {toDo?.id}
-    </>
+    <Card>
+      <Header>
+        <p>ID : {toDo?.id}</p>
+        <Link to={`/`}>
+          <button>이전으로</button>
+        </Link>
+      </Header>
+      <h4>{toDo?.title}</h4>
+      <p>{toDo?.body}</p>
+    </Card>
   );
 }
 
--- src/store.js
+++ src/store.js
@@ -1,72 +1,38 @@
 // import { createStore } from "redux";
-import { configureStore, createAction, createReducer, createSlice } from "@reduxjs/toolkit";
-
-// 문자열 형태로 reducer 함수에 직접 사용할 수도 있지만 변수로 저장함으로써 디버깅 과정에서 에러를 쉽게 색출하기 위해
-// const ADD = "ADD";
-// const DELETE = "DELETE";
-
-// 마찬가지로 dispatch 함수로 설정해서 사용할 수 있지만 변수로 저장함으로써 코드의 간략화와 불필요한 재사용을 줄일 수 있다
-// const addToDo = (text) => {
-//   return {
-//     type: ADD,
-//     text,
-//   };
-// };
-
-// const deleteToDo = (id) => {
-//   return {
-//     type: DELETE,
-//     id: parseInt(id),
-//   };
-// };
-
-// React에서 중요한 "불변성"을 지키는 것과 마찬가지로 Redux에서도 Store에 저장된 데이터의 불변성을 지키기 위해
-// 데이터를 변형(mutation)하지 않고 새로운 배열을 생성해서 처리되도록 코드를 작성해야 한다
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case addToDo.type:
-//       return [{ text: action.payload, id: Date.now() }, ...state];
-//     case deleteToDo.type:
-//       return state.filter((todo) => todo.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// };
-
-/*  
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
-
-
-// createReducer는 State를 Mutate(변형)도 가능하고, 새로운 배열을 생성해서 처리해도 된다.
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({
-      text: action.payload,
-      id: Date.now(),
-    });
-  },
-  [deleteToDo]: (state, action) => {
-    state.filter((todo) => todo.id !== action.payload);
-  },
-});
-
-*/
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const toDos = createSlice({
   name: "toDosReducer",
-  initialState: [],
+  initialState: [
+    {
+      id: 0, // id는 모두 고유값이어야 합니다.
+      title: "리액트 강의보기",
+      body: "챕터 1부터 챕터 12까지 학습",
+      isDone: false,
+    },
+    {
+      id: 1, // id는 모두 고유값이어야 합니다.
+      title: "점심 먹기",
+      body: "점심 뭐먹지..?",
+      isDone: false,
+    },
+  ],
   reducers: {
     add: (state, action) => {
       state.push({
-        text: action.payload,
-        id: Date.now(),
+        id: action.payload.id,
+        title: action.payload.title,
+        body: action.payload.body,
+        isDone: action.payload.isDone,
       });
     },
     remove: (state, action) => state.filter((todo) => todo.id !== action.payload),
+    toggleStatusTodo: (state, action) => {
+      state[action.payload].isDone = !state[action.payload].isDone;
+    },
   },
 });
 
-export const { add, remove } = toDos.actions;
+export const { add, remove, toggleStatusTodo } = toDos.actions;
 
 export default configureStore({ reducer: toDos.reducer });
